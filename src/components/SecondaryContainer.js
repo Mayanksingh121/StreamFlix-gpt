55dd1798393041884a28cfa6ae655999f7a9bf1f
Mayank Singh <mt029322@gmail.com>
2024-05-31 10:02:59 +0530

    fixed videobg and mainContainer

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -6,7 +6,7 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
     <div className="bg-black">
-      <div className="md:-mt-32 lg:-mt-40 z-40 relative">
+      <div className="md:-mt-32 lg:-mt-20 z-40 relative">
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Upcoming"} movies={movies.upcomingMovies} />
         <MovieList title={"Popular"} movies={movies.popularMovies} />
